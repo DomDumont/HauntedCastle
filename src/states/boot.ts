daf875d0fd3f6a895969c82933938dd6defbf507
DomDom <dom.dumont@gmail.com>
2017-12-05 11:58:12 +0100

    add 2 example images

--- src/states/boot.ts
+++ src/states/boot.ts
@@ -1,9 +1,9 @@
 
-
+let img_bunny: any = require("../public/data/images/zelda1.png");
 export default class Boot extends Phaser.State {
     public preload(game: Phaser.Game): void{
         console.log("preload");
-        game.load.image("einstein", "assets/pics/ra_einstein.png");
+        game.load.image("einstein", img_bunny);
     } 
     public create(game: Phaser.Game): void {
         const s = game.add.sprite(80, 0, "einstein");
