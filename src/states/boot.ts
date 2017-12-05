46546d1172e8837a5db4edf112d515a428810c5b
DomDom <dom.dumont@gmail.com>
2017-12-05 11:49:32 +0100

    now phaser is not included in bundle.js anymore ans it works. add the .map also

--- src/states/boot.ts
+++ src/states/boot.ts
@@ -1,5 +1,13 @@
 
 
 export default class Boot extends Phaser.State {
-
+    public preload(game: Phaser.Game): void{
+        console.log("preload");
+        game.load.image("einstein", "assets/pics/ra_einstein.png");
+    } 
+    public create(game: Phaser.Game): void {
+        const s = game.add.sprite(80, 0, "einstein");
+        
+        s.rotation = 0.14;
+    }
 }
